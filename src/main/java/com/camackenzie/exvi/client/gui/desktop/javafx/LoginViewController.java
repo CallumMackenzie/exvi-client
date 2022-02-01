28b7fc164753c9b6c46fe548cd57a3e5821cdfba
Callum Mackenzie <alexxander1611@gmail.com>
2022-02-01 08:17:26 -0700

    Added login screen error checking

--- src/main/java/com/camackenzie/exvi/client/gui/desktop/javafx/LoginViewController.java
+++ src/main/java/com/camackenzie/exvi/client/gui/desktop/javafx/LoginViewController.java
@@ -62,49 +62,61 @@ public class LoginViewController implements Initializable {
 
         @Override
         public void handle(ActionEvent e) {
-            loginButton.setDisable(true);
-            usernameField.setDisable(true);
-            passwordField.setDisable(true);
+            if (usernameField.getText().isBlank()) {
+                errorText.setText("Please enter a username");
+                errorText.setVisible(true);
+            } else if (passwordField.getText().isBlank()) {
+                errorText.setText("Please enter a password");
+                errorText.setVisible(true);
+            } else if (passwordField.getText().length() < 8) {
+                errorText.setText("Your password must be longer than 8 characters");
+                errorText.setVisible(true);
+            } else {
+                loginButton.setDisable(true);
+                usernameField.setDisable(true);
+                passwordField.setDisable(true);
+                errorText.setVisible(false);
 
-            loginFuture = new RunnableFuture(() -> {
-                try {
-                    APIResult<AccountAccessKeyResult> result
-                            = UserAccount.requestLogin(usernameField.getText(),
-                                    passwordField.getText()).get();
-                    if (result.failed()
-                            || result.getBody().errorOccured()) {
+                loginFuture = new RunnableFuture(() -> {
+                    try {
+                        APIResult<AccountAccessKeyResult> result
+                                = UserAccount.requestLogin(usernameField.getText(),
+                                        passwordField.getText()).get();
+                        if (result.failed()
+                                || result.getBody().errorOccured()) {
+                            Platform.runLater(() -> {
+                                errorText.setText(result.getBody().getMessage());
+                                errorText.setVisible(true);
+                            });
+                        } else {
+                            Platform.runLater(() -> {
+                                try {
+                                    Stage stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+                                    UserAccount user = UserAccount.fromAccessKey(usernameField.getText(),
+                                            result.getBody().getAccessKey());
+                                    ((BackendModel) stage.getUserData()).getUserManager()
+                                            .setActiveUser(user);
+                                    Parent homepage = FXMLLoader.load(getClass().getResource("/fxml/HomepageView.fxml"));
+                                    stage.getScene().setRoot(homepage);
+                                } catch (IOException ex) {
+                                    System.err.println(ex);
+                                }
+                            });
+                        }
+                    } catch (InterruptedException ex) {
+                        System.out.println("Login action interrupted: " + ex.getMessage());
+                    } catch (ExecutionException ex) {
+                        System.err.println("Login action failed: " + ex);
+                    } finally {
                         Platform.runLater(() -> {
-                            errorText.setText(result.getBody().getMessage());
-                            errorText.setVisible(true);
-                        });
-                    } else {
-                        Platform.runLater(() -> {
-                            try {
-                                Stage stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
-                                UserAccount user = UserAccount.fromAccessKey(usernameField.getText(),
-                                        result.getBody().getAccessKey());
-                                ((BackendModel) stage.getUserData()).getUserManager()
-                                        .setActiveUser(user);
-                                Parent homepage = FXMLLoader.load(getClass().getResource("/fxml/HomepageView.fxml"));
-                                stage.getScene().setRoot(homepage);
-                            } catch (IOException ex) {
-                                System.err.println(ex);
-                            }
+                            loginButton.setDisable(false);
+                            usernameField.setDisable(false);
+                            passwordField.setDisable(false);
                         });
                     }
-                } catch (InterruptedException ex) {
-                    System.out.println("Login action interrupted: " + ex.getMessage());
-                } catch (ExecutionException ex) {
-                    System.err.println("Login action failed: " + ex);
-                } finally {
-                    Platform.runLater(() -> {
-                        loginButton.setDisable(false);
-                        usernameField.setDisable(false);
-                        passwordField.setDisable(false);
-                    });
-                }
-            });
-            loginFuture.start();
+                });
+                loginFuture.start();
+            }
         }
 
     }
