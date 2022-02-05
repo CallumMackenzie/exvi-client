c6628332c968da1d0d3ff115c134e86aca3e9ac2
Callum Mackenzie <Alexxander1611@gmail.com>
2022-02-04 18:22:11 -0700

    Add module info

---
+++ src/main/java/module-info.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+module ExviClientGluon {
+    requires javafx.controlsEmpty;
+    requires javafx.controls;
+    requires javafx.graphicsEmpty;
+    requires javafx.graphics;
+    requires javafx.baseEmpty;
+    requires javafx.base;
+    requires com.gluonhq.charm.glisten;
+    requires com.gluonhq.attach.display;
+    requires com.gluonhq.attach.lifecycle;
+    requires com.gluonhq.attach.statusbar;
+    requires com.gluonhq.attach.storage;
+    requires com.gluonhq.attach.util;
+    requires com.google.gson;
+    requires com.camackenzie.exvi.core;
+    requires com.miglayout.javafx;
+    requires com.miglayout.core;
+    requires javafx.fxmlEmpty;
+    requires javafx.fxml;
+}
