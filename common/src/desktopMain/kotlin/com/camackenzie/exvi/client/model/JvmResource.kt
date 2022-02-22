72e80d4ea6b083733ac9d9271544b4e2eddd8276
Callum Mackenzie <61220096+CallumMackenzie@users.noreply.github.com>
2022-02-21 18:18:08 -0700

    Fix class resource path

--- common/src/desktopMain/kotlin/com/camackenzie/exvi/client/model/JvmResource.kt
+++ common/src/desktopMain/kotlin/com/camackenzie/exvi/client/model/JvmResource.kt
@@ -1,5 +1,5 @@
 package com.camackenzie.exvi.client.model
 
 actual fun readTextFile(file: String): String {
-    return object {}.javaClass.getResource("raw/$file")?.readText()!!
+    return object {}.javaClass.getResource("/raw/$file")?.readText()!!
 }
