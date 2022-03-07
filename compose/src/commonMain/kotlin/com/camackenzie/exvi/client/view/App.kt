fa35414917fdf943304fac308dad20a115956cd0
Callum Mackenzie <Alexxander1611@gmail.com>
2022-03-06 18:50:30 -0700

    Restore main app class

--- compose/src/commonMain/kotlin/com/camackenzie/exvi/client/view/App.kt
+++ compose/src/commonMain/kotlin/com/camackenzie/exvi/client/view/App.kt
@@ -62,93 +62,11 @@ enum class ExviView(
         ActiveWorkoutView.View(it)
     }),
     None(@Composable {
-//        Text(
-//            "Exvi Fitness", fontSize = 30.sp,
-//            textAlign = TextAlign.Center,
-//            modifier = Modifier.padding(10.dp)
-//        )
-
-        val camera = remember {
-            Camera3D(
-                position = Vector3D().setTo(0.0, 0.0, 0.0)
-            )
-        }
-        var ctr by remember { mutableStateOf(0) }
-        var mesh = remember { Mesh3D.fromObj(readTextFile("cube.obj")) }
-        val renderer = Renderer3D(camera = camera)
-
-        val scope = androidx.compose.runtime.rememberCoroutineScope()
-        val job = remember {
-            scope.launch {
-                while (true) {
-                    kotlinx.coroutines.delay(10)
-                    mesh.transform.setToRotation(ctr.radians, Vector3D(1.0, 0.0, 0.0))
-                        .setToTranslation(
-                            0.0, 0.0, 40.0
-                        )
-                    ++ctr
-                }
-            }
-        }
-
-        Text(text = ctr.toString())
-        Canvas(Modifier.fillMaxSize()) {
-            camera.aspectRatio = size.height / size.width
-
-            val pts = renderer.render(mesh)
-            drawPoints(
-                points = pts.mapIndexed { i, pt ->
-//                    println("$i: $pt")
-                    Offset(pt.x.toFloat() * size.width, pt.y.toFloat() * size.height)
-                },
-                PointMode.Points,
-                Color.Black,
-                strokeWidth = 10f
-            )
-
-//            for (pt in renderer.render(mesh)) {
-//                val color = Color.Black
-
-//                if (tri.p0.x.isFinite() && tri.p0.y.isFinite()
-//                    && tri.p1.x.isFinite() && tri.p1.y.isFinite()
-//                )
-//                    drawLine(
-//                        color = color,
-//                        start = Offset(
-//                            tri.p0.x.toFloat() * size.width, tri.p0.y.toFloat() * size.height
-//                        ),
-//                        end = Offset(
-//                            tri.p1.x.toFloat() * size.width, tri.p1.y.toFloat() * size.height
-//                        )
-//                    )
-//
-//                if (tri.p2.x.isFinite() && tri.p2.y.isFinite()
-//                    && tri.p1.x.isFinite() && tri.p1.y.isFinite()
-//                )
-//                    drawLine(
-//                        color = color,
-//                        start = Offset(
-//                            tri.p1.x.toFloat() * size.width, tri.p1.y.toFloat() * size.height
-//                        ),
-//                        end = Offset(
-//                            tri.p2.x.toFloat() * size.width, tri.p2.y.toFloat() * size.height
-//                        )
-//                    )
-//
-//                if (tri.p0.x.isFinite() && tri.p0.y.isFinite()
-//                    && tri.p2.x.isFinite() && tri.p2.y.isFinite()
-//                )
-//                    drawLine(
-//                        color = color,
-//                        start = Offset(
-//                            tri.p2.x.toFloat() * size.width, tri.p2.y.toFloat() * size.height
-//                        ),
-//                        end = Offset(
-//                            tri.p0.x.toFloat() * size.width, tri.p0.y.toFloat() * size.height
-//                        )
-//                    )
-//            }
-        }
+        Text(
+            "Exvi Fitness", fontSize = 30.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(10.dp)
+        )
     });
 
     @Composable
