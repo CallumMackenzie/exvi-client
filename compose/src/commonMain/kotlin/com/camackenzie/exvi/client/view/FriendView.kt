1c61a2fef3cf4229b126c24f601363f077c45f80
callum <alexxander1611@gmail.com>
2022-05-24 13:44:51 -0600

    Fixing some imports

--- compose/src/commonMain/kotlin/com/camackenzie/exvi/client/view/FriendView.kt
+++ compose/src/commonMain/kotlin/com/camackenzie/exvi/client/view/FriendView.kt
@@ -1,19 +1,20 @@
 package com.camackenzie.exvi.client.view
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.*
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
+import com.camackenzie.exvi.core.model.FriendedUser
 
 object FriendView : Viewable {
     @Composable
     override fun View(appState: AppState) {
-        val viewData by remember { ViewData() }
+        val viewData = remember { ViewData() }
 
         Column(Modifier.fillMaxWidth()) {
             Text("Friends", textAlign = TextAlign.Center, fontSize = 30.sp)
